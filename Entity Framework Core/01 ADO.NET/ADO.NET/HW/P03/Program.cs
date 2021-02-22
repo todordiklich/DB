598ff308b282b63302be6f4188d382760043efcb
Todor Diklich <todordiklich@gmail.com>
2021-02-22 18:25:40 +0200

    Adding more resources

---
+++ Entity Framework Core/01 ADO.NET/ADO.NET/HW/P03/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace P03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=.;Database=MinionsDB;Integrated Security=true";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string nameQuery = @"SELECT Name FROM Villains WHERE Id = @Id";
+                int id = int.Parse(Console.ReadLine());
+
+                using (var nameCommand = new SqlCommand(nameQuery, connection))
+                {
+                    nameCommand.Parameters.AddWithValue("@Id", id);
+                    string villainName = nameCommand.ExecuteScalar() as string;
+
+                    if (villainName == null)
+                    {
+                        Console.WriteLine($"No villain with ID {id} exists in the database.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Villain: {villainName}");
+
+                        string minionsQuery = @"SELECT ROW_NUMBER() OVER (ORDER BY m.Name) as RowNum,
+                                         m.Name, 
+                                         m.Age
+                                    FROM MinionsVillains AS mv
+                                    JOIN Minions As m ON mv.MinionId = m.Id
+                                   WHERE mv.VillainId = @Id
+                                ORDER BY m.Name";
+
+                        using (var minionsCommand = new SqlCommand(minionsQuery, connection))
+                        {
+                            minionsCommand.Parameters.AddWithValue("@Id", id);
+                            using (var reader = minionsCommand.ExecuteReader())
+                            {
+                                if (reader.HasRows)
+                                {
+                                    while (reader.Read())
+                                    {
+                                        Console.WriteLine($"{reader[0]}. {reader[1]} {reader[2]}");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("(no minions)");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Entity Framework Core/01 ADO.NET/ADO.NET/HW/P06/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace P06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=.;Database=MinionsDB;Integrated Security=true";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string villainQuery = @"SELECT Name FROM Villains WHERE Id = @villainId";
+                int id = int.Parse(Console.ReadLine());
+
+                using (var villainCommand = new SqlCommand(villainQuery, connection))
+                {
+                    villainCommand.Parameters.AddWithValue("@villainId", id);
+                    string villainName = villainCommand.ExecuteScalar() as string;
+
+                    if (villainName == null)
+                    {
+                        Console.WriteLine("No such villain was found.");
+                    }
+                    else
+                    {
+                        string minionsReleasedQuery = @"DELETE FROM MinionsVillains 
+      WHERE VillainId = @villainId";
+
+                        int releasedCount = 0;
+                        using (var minionsCommand = new SqlCommand(minionsReleasedQuery, connection))
+                        {
+                            minionsCommand.Parameters.AddWithValue("@villainId", id);
+                            releasedCount = minionsCommand.ExecuteNonQuery();
+                        }
+                        string villainDeleteQuery = @"DELETE FROM Villains
+      WHERE Id = @villainId";
+
+                        using (var villainDeleteCommand = new SqlCommand(villainDeleteQuery, connection))
+                        {
+                            villainDeleteCommand.Parameters.AddWithValue("@villainId", id);
+                            villainDeleteCommand.ExecuteNonQuery();
+
+                            Console.WriteLine($"{villainName} was deleted.");
+                            Console.WriteLine($"{releasedCount} minions were released.");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
