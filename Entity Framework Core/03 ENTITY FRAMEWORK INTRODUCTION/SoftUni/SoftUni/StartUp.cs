9ef592b01870db6ec1d1b5a5f141530ef4c558b4
Todor Diklich <todordiklich@gmail.com>
2021-02-24 21:33:51 +0200

    EntityFrameworkCore Introduction

---
+++ Entity Framework Core/03 ENTITY FRAMEWORK INTRODUCTION/SoftUni/SoftUni/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var db = new SoftUniContext();
+            var result = GetEmployeesFullInformation(db);
+            Console.WriteLine(result);
+        }
+
+        // problem 3
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var empFullInfo = context.Employees.Select(x => new
+            {
+                x.FirstName,
+                x.LastName,
+                x.MiddleName,
+                x.JobTitle,
+                x.Salary,
+                x.EmployeeId
+            })
+                .OrderBy(x => x.EmployeeId)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var employee in empFullInfo)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
